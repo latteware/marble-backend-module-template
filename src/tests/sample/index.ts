073d4feaf984f7ab6a62e70e2db8d628c74f7cc0
Siedrix Zavala <Siedrix@gmail.com>
2023-07-17 13:12:53 -0600

    Migrate to typescript. Change to a base class on index.ts.

---
+++ src/tests/sample/index.ts
@@ -0,0 +1,44 @@
+/* global describe, it */
+import { expect } from 'chai'
+
+import { Sample } from '../../index'
+
+describe('Sample', function () {
+  it('Should return config', async function () {
+    const sample = new Sample({
+      name: 'sample',
+      age: 18
+    })
+
+    const config = sample.getConfig()
+
+    expect(config.name).to.equal('sample')
+    expect(config.age).to.equal(18)
+  })
+
+  it('Should update config', async function () {
+    const sample = new Sample({
+      name: 'sample',
+      age: 18
+    })
+
+    sample.setConfig({
+      name: 'sample',
+      age: 19
+    })
+
+    const config = sample.getConfig()
+
+    expect(config.name).to.equal('sample')
+    expect(config.age).to.equal(19)
+  })
+
+  it('Should be able to initialize without config', async function () {
+    const sample = new Sample()
+
+    const config = sample.getConfig()
+
+    expect(config.name).to.equal('sample')
+    expect(config.age).to.equal(18)
+  })
+})
